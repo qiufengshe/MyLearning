a02ded742ad691f034ea4b8343da965b124830a3
qiufengshe <sunqiusheng827@126.com>
2021-07-11 17:12:36 +0800

    1. c# 新语法is not null 和 is和模式匹配

---
+++ src/CharpLearning/Syntax/LearnIsNotNull.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CharpLearning.Syntax
+{
+    /// <summary>
+    /// is not null 语法糖
+    /// 在c# 9 加入
+    /// </summary>
+    public class LearnIsNotNull
+    {
+        public void IsNotNull(object obj)
+        {
+            //之前
+            if (obj != null)
+            {
+                Console.WriteLine("obj != null");
+            }
+
+            //现在可以用,is not null只是语法糖,在编译时,会替换成"!=null"
+            if (obj is not null)
+            {
+                Console.WriteLine("obj is not null");
+            }
+
+            //obj is not null 反编译 
+            /*
+             if (obj != null)
+		     {
+			      Console.WriteLine("obj is not null");
+		     }
+             */
+        }
+    }
+}
---
+++ src/CharpLearning/Syntax/LearnPatternMatch.cs
@@ -0,0 +1,41 @@
+﻿namespace CharpLearning.Syntax
+{
+	/// <summary>
+	/// 增强的模式匹配 和is一起使用
+	/// c# 9 加入,其实也是语法糖
+	/// </summary>
+    public class LearnPatternMatch
+	{
+		public bool IsAlpha(char c)
+		{
+			return c is >= 'a' and <= 'z' or >= 'A' and <= 'Z';
+
+            /*
+			 反编译结果:
+			    bool flag;
+                bool flag1;
+                if (c >= 'a')
+                {
+                    if (c <= 'z')
+                    {
+                        flag = true;
+                        flag1 = flag;
+                        return flag1;
+                    }
+                }
+                else if (c >= 'A')
+                {
+                    if (c <= 'Z')
+                    {
+                        flag = true;
+                        flag1 = flag;
+                        return flag1;
+                    }
+                }
+                flag = false;
+                flag1 = flag;
+                return flag1;
+			 */
+        }
+    }
+}
