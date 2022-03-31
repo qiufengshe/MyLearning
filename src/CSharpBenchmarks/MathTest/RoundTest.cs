10f2bcb6991d342a338575a3eaae2d8cba40005f
qiufengshe <172344058@qq.com>
2022-03-31 21:59:41 +0800

    加入Math.Round性能基准测试

---
+++ src/CSharpBenchmarks/MathTest/RoundTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.MathTest
+{
+    [DisassemblyDiagnoser(printSource: true, maxDepth: 3)]
+    [MemoryDiagnoser]
+    public class RoundTest
+    {
+        [Params(1.5, 2.5, 3.5)]
+        public double Value { get; set; }
+
+        [Benchmark]
+        public double ToEvenTest() => Math.Round(Value, MidpointRounding.ToEven);
+
+
+        [Benchmark]
+        public double AwayFromZeroTest() => Math.Round(Value, MidpointRounding.AwayFromZero);
+
+
+        [Benchmark(Baseline = true)]
+        public double RoundDefault() => Math.Round(Value);
+    }
+}
