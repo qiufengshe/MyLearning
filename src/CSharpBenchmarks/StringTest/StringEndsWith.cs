ca33c410f1337f2ba01d8e53667fadfdcf55fa5c
qiufengshe <172344058@qq.com>
2022-06-22 22:23:59 +0800

    在.Net 7 String的StartsWith和EndsWith性能优化

---
+++ src/CSharpBenchmarks/StringTest/StringEndsWith.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.StringTest
+{
+    [MemoryDiagnoser]
+    [DisassemblyDiagnoser(printSource: true)]
+    public class StringEndsWith
+    {
+        string value = "this, is, a, very long string, with some spaces, commas and more spaces";
+
+        [Params(1024, 2048)]
+        public int Times { get; set; }
+
+        [Params('t', 's')]
+        public char Chars { get; set; }
+
+        [Benchmark]
+        public void EndsWithTest()
+        {
+            int count = 0;
+            for (int i = 0; i < Times; i++)
+            {
+                if (value.EndsWith(Chars))
+                {
+                    count += 1;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void StartsWithTest()
+        {
+            int count = 0;
+            for (int i = 0; i < Times; i++)
+            {
+                if (value.StartsWith(Chars))
+                {
+                    count += 1;
+                }
+            }
+        }
+    }
+}
