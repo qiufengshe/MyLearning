b5861d082828d68a557b55face6099b5e2744509
qiufengshe <172344058@qq.com>
2023-07-18 21:09:40 +0800

    add inline array struct

---
+++ src/CSharpBenchmarks/ArrayTest/InlineArrayTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.ArrayTest
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class InlineArrayTest
+	{
+	}
+#if NET8_0_OR_GREATER
+	[InlineArray(TestLength)]
+	public struct Test1
+	{
+		public const int TestLength = 10;
+		public int x;
+	}
+#endif
+}
