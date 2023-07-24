0f5150c56ef0612e80ffbdd6e83313f748d3ff2a
qiufengshe <172344058@qq.com>
2023-07-24 22:26:23 +0800

    内插字符串测试

---
+++ src/CSharpBenchmarks/StringTest/StringInterpolated.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBenchmarks.StringTest
+{
+public class StringInterpolated
+{
+	public string Test1()
+	{
+		int a = 20;
+		return $"age={a}";  //这种方式就是内插字符串
+	}
+
+	public string Test2()
+	{
+		int a = 20;
+		return string.Format("age={0}", a); //在字符串比较短的时候,可以使用,
+	}
+
+	public string Test3()
+	{
+		int a = 20;
+		return "age=" + a;  //不建议使用这种方式,进行字符串拼接
+	}
+}
+}
---
+++ src/CharpLearning/Syntax/InterpolatedStringTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharpLearning.Syntax
+{
+    public class InterpolatedStringTest
+    {
+        public string Test1()
+        {
+            int a = 20;
+            string result = $"a={a}";
+            return result;
+        }
+    }
+}
