0dcdfd694f859e56f46cfa52fed8da6a00f06546
qiufengshe <172344058@qq.com>
2023-07-24 22:27:31 +0800

    Unroll StringBuilder.Append

---
+++ src/CSharpBenchmarks/StringTest/StringBuilderTest.cs
@@ -0,0 +1,19 @@
+﻿using System.CodeDom;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.StringTest
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class StringBuilderTest
+	{
+		[Benchmark]
+		public string Append()
+		{
+			StringBuilder sb = new StringBuilder(16);
+			sb.Append("hello world!");
+			return sb.ToString();
+		}
+	}
+}
