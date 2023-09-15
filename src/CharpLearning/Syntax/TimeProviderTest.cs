dc6e898755eb7465dc2db9a2ce4a2bf7de9072c4
qiufeng <172344058@qq.com>
2023-09-15 11:30:05 +0800

    Add TimeProvider Test Method

---
+++ src/CharpLearning/Syntax/TimeProviderTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace CharpLearning.Syntax
+{
+    public class TimeProviderTest
+    {
+        public void Test()
+        {
+            //TimeProvider.System.CreateTimer() //创建一个定时器
+            var timeProvider = TimeProvider.System;
+            var start = timeProvider.GetTimestamp();
+
+            Thread.Sleep(3000);
+
+            var timeSpan = timeProvider.GetElapsedTime(start);
+            Console.WriteLine($"{timeSpan.Milliseconds}毫秒数");
+
+        }
+    }
+}
