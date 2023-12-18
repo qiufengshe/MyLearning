a10eaddd3da342a9849720eaaaf93ffeef870299
qiufengshe <172344058@qq.com>
2023-12-18 21:28:11 +0800

    csharp enum test

---
+++ src/CSharpBenchmarks/EnumTest/EnumTest.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.EnumTest
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class EnumTest
+	{
+		[Params(16, 128)]
+		public int Count { get; set; }
+
+		[Params(Status.Initial, Status.Processing, Status.Finished)]
+		public Status RecordStatus { get; set; }
+
+		[Benchmark]
+		public void HasFlag()
+		{
+			int sum = 0;
+			for (int i = 0; i < Count; i++)
+			{
+				Status status = RecordStatus;
+				if (status.HasFlag(Status.Initial))
+				{
+					sum += 1;
+				}
+			}
+		}
+	}
+
+	public enum Status : byte
+	{
+		Initial = 0,
+
+		Processing = 1,
+
+		Finished = 2
+	}
+
+}
