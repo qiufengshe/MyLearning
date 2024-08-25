4fec7432c86ddcdd5db9d24fa803f9f5af50e8a7
qiufeng <172344058@qq.com>
2024-08-25 21:49:42 +0800

    new synatx

---
+++ src/CSharpBenchmarks/ArrayTest/ArrayInitTest.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.ArrayTest
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class ArrayInitTest
+	{
+		[Benchmark(Baseline = true)]
+		public string[] Old()
+		{
+			return new string[] { "hello", "wolrd", "test" };
+		}
+
+		[Benchmark]
+		public string[] New()
+		{
+			return ["aaaaa", "bbbbb", "cccc"];
+		}
+
+	}
+}
---
+++ src/CSharpBenchmarks/ArrayTest/ListInitTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.ArrayTest
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class ListInitTest
+	{
+		[Benchmark(Baseline = true)]
+		public List<string> Old()
+		{
+			return ["hello", "wolrd", "test"];
+		}
+
+		public List<string> New()
+		{
+			return ["aaaaa", "bbbbb", "cccc"];
+		}
+	}
+}
