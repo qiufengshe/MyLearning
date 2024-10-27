24afb13862f3a5633d2ae7fd0a65c8f666b262c3
qiufeng <172344058@qq.com>
2024-10-27 20:34:33 +0800

    reflection test

--- src/CSharpBenchmarks/Syntax/ReflectionTest.cs
+++ src/CSharpBenchmarks/Syntax/ReflectionTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using Bogus;
@@ -9,6 +12,8 @@ namespace CSharpBenchmarks.Syntax
 	[MemoryDiagnoser]
 	public class ReflectionTest
 	{
+		private static Func<HttpContent, bool>? isBuffered;
+
 		[Params(128)]
 		public int Count { get; set; }
 
@@ -36,6 +41,15 @@ namespace CSharpBenchmarks.Syntax
 			}
 		}
 
+		public void ExpressionTest()
+		{
+			var property = typeof(HttpContent).GetProperty("IsBuffered", BindingFlags.NonPublic | BindingFlags.Instance);
+			if (property != null)
+			{
+				isBuffered = CreateGetFunc<HttpContent, bool>(property.DeclaringType, property.Name, property.PropertyType);
+			}
+		}
+
 		public static int ReflectionTest1(Person person)
 		{
 			var field = typeof(Person).GetField("age", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -45,5 +59,21 @@ namespace CSharpBenchmarks.Syntax
 
 		[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "age")]
 		public extern static ref int GetAge(Person person);
+
+		public static Func<TDeclaring, TProperty> CreateGetFunc<TDeclaring, TProperty>(Type declaringType, string propertyName, Type? propertyType = null)
+		{
+			var paramInstance = Expression.Parameter(typeof(TDeclaring));
+			var bodyInstance = (Expression)paramInstance;
+			if (typeof(TDeclaring) != declaringType)
+			{
+				bodyInstance = Expression.Convert(bodyInstance, declaringType);
+			}
+			var bodyProperty = (Expression)Expression.Property(bodyInstance, propertyName);
+			if (typeof(TProperty) != propertyType)
+			{
+				bodyProperty = Expression.Convert(bodyProperty, propertyType);
+			}
+			return Expression.Lambda<Func<TDeclaring, TProperty>>(bodyProperty, paramInstance).Compile();
+		}
 	}
 }
