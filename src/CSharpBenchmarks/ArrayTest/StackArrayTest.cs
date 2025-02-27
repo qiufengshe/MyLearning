9c5515b17ce8b55cfb2a1700643bc1aa428fab5a
qiufengshe <172344058@qq.com>
2025-02-27 13:32:57 +0800

    add refalection test

---
+++ src/CSharpBenchmarks/ArrayTest/StackArrayTest.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.ArrayTest
+{
+	//dotnet run -c Release -f net9.0 --filter="*StackArrayTest*" --runtimes net9.0 net10.0
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class StackArrayTest
+	{
+		[Benchmark]
+		public int SumTest()
+		{
+			int[] arr = [1, 2, 3, 5];
+			int sum = 0;
+			foreach (var num in arr)
+			{
+				sum += num;
+			}
+			return sum;
+		}
+	}
+}
