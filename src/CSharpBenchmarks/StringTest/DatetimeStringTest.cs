4c6d44e62c69459a806fd28f17e6506e5a885072
qiufengshe <172344058@qq.com>
2026-04-07 13:49:00 +0800

    add datetime test

---
+++ src/CSharpBenchmarks/StringTest/DatetimeStringTest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text.Json;
+using BenchmarkDotNet.Attributes;
+
+
+[DisassemblyDiagnoser(printSource: true, maxDepth: 3)]
+[MemoryDiagnoser]
+public class DatetimeStringTest
+{
+	[Params(128, 1024)]
+	public int Count { get; set; }
+
+
+	[Benchmark(Baseline = true)]
+	public void Deconstruct()
+	{
+		var sum = 0;
+		for (var i = 0; i < Count; i++)
+		{
+			var ((year, month, day), (hour, minute, second)) = DateTime.Now;
+			var s = $"{year}/{month}/{day} {hour}:{minute}:{second}";
+			sum += s.Length;
+		}
+	}
+
+	[Benchmark]
+	public void String()
+	{
+		var sum = 0;
+		for (var i = 0; i < Count; i++)
+		{
+			var s = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+			sum += s.Length;
+		}
+	}
+}
