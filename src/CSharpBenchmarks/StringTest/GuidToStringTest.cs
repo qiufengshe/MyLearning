219a0f97d0d9e5c3fdbf7d27836fbc8861c2101d
qiufeng <172344058@qq.com>
2026-04-16 22:38:39 +0800

    add guid format test

---
+++ src/CSharpBenchmarks/StringTest/GuidToStringTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarks.StringTest
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printSource: true)]
+	public class GuidToStringTest
+	{
+		[Params(128, 1024)]
+		public int Count { get; set; }
+
+		[Benchmark(Baseline = true)]
+		public void ToString1()
+		{
+			var sum = 0;
+			for (int i = 0; i < Count; i++)
+			{
+				var s = Guid.NewGuid().ToString().Replace("-", "");
+				sum += s.Length;
+			}
+		}
+
+		[Benchmark]
+		public void ToString2()
+		{
+			var sum = 0;
+			for (int i = 0; i < Count; i++)
+			{
+				var s = Guid.NewGuid().ToString("N");
+				sum += s.Length;
+			}
+		}
+	}
+}
